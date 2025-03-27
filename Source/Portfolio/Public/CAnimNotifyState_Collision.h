84b0484ec008009e0af376b2b8f134ed3f05e625
leejoonsoo1 <52502579+leejoonsoo1@users.noreply.github.com>
2025-03-27 18:07:55 +0900

    2025 03 27
    몬스터의 Collision On, Off를 담당하는 AnimNotifyState 구현중

--- Source/Portfolio/Public/CAnimNotifyState_Collision.h
+++ Source/Portfolio/Public/CAnimNotifyState_Collision.h
@@ -4,9 +4,29 @@
 #include "Animation/AnimNotifies/AnimNotifyState.h"
 #include "CAnimNotifyState_Collision.generated.h"
 
+/*
+*	2025 03 27
+*	Collision 클래스
+*	몬스터에 ㄱ
+*/
+
 UCLASS()
 class PORTFOLIO_API UCAnimNotifyState_Collision : public UAnimNotifyState
 {
 	GENERATED_BODY()
 	
+public:
+	virtual FString GetNotifyName_Implementation() const override;
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Collision", meta = (AllowPrivateAccess = "true"))
+	bool HeadCollision = false;
+
+	UPROPERTY(EditAnywhere, Category = "Collision", meta = (AllowPrivateAccess = "true"))
+	bool TailCollision = false;
+
+	UPROPERTY(EditAnywhere, Category = "Collision", meta = (AllowPrivateAccess = "true"))
+	bool FeetCollision = false;
 };
